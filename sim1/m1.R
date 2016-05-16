97d956ac2ccd1a15167bad5de5739f16e74eb46b
Jarad Niemi <niemi@iastate.edu>
2016-05-16 13:17:15 -0500

    Ignore sim/ directory

---
+++ sim1/m1.R
@@ -0,0 +1,13 @@
+# R object passed from make file:
+#    i:      simulation id
+method_name = "m1"                # Do NOT use "sum"
+
+d = readRDS(file=paste0("sim/sim-",i,".rds"))              # Don't change this line
+
+##################################################################################
+# Put method here with results called 'inf'
+inf = rnorm(10, mean(d)+1, 1) # Fake MCMC samples
+
+##################################################################################
+
+saveRDS(inf, file=paste0("sim/inf-",method_name,"-",i,".rds")) # Don't change this line
---
+++ sim1/sim.R
@@ -0,0 +1,1 @@
+saveRDS(rnorm(10), file=paste0("sim/sim-",i,".rds"))
