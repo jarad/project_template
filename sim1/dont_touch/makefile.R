637896ff4dece4326e3011815d3779074353d98f
Jarad Niemi <niemi@iastate.edu>
2016-05-18 17:28:12 -0500

    Remove duplicate $(R) definition

--- sim1/dont_touch/makefile.R
+++ sim1/dont_touch/makefile.R
@@ -54,7 +54,7 @@ cat("####################################################\n")
 
 # Slurm cluster
 if (Sys.getenv("SLURM_JOB_ID")!="") {
-	cat("SHELL=srun \n.SHELLFLAGS= -N1 -n1 \nR=$(SHELL) $(.SHELLFLAGS) \nR=$(SHELL) $(.SHELLFLAGS) module load R; \n\n\n")
+	cat("SHELL=srun \n.SHELLFLAGS= -N1 -n1 \nR=$(SHELL) $(.SHELLFLAGS) module load R; \n\n\n")
 }
 cat("R+= Rscript -e\n\n")
 
